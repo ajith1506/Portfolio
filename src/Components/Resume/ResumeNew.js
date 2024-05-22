ee8ef12f30ffa76b67c1052ed42271c7c2b365a3
ajith1506 <ajithkumararun1111@gmail.com>
2024-05-22 15:48:25 +0530

    change

--- src/Components/Resume/ResumeNew.js
+++ src/Components/Resume/ResumeNew.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
@@ -6,17 +6,11 @@ import pdf from "../../Assets/Ajith-V-Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { pdfjs } from "react-pdf";
 import "./resume.css";
-import photo from "../../Assets/ajith.resume.png";
+import photo from "../../Assets/ajith-resume.png";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
     <div>
       <Container fluid className="resume-section">
